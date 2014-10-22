2f6d0850a01612044b2b45e79e3b88debcfbc097
kota615 <21111153ks@tama.ac.jp>
2014-10-22 16:29:53 +0900

    計算追加しました

---
+++ keisann.java
@@ -0,0 +1,28 @@
+class keisan
+{
+	public static void main(string argv[])
+	{
+		system.out.println("hello,world!")
+		// “ü—ÍF2‚Â‚Ì”’l‚ğ“ü—Í
+		int in0= new java.utill.scanner(system.in).nextInt();
+		int in1= new java.utill.scanner(system.in).nextInt();
+		system.out.printIn(in0+","+in1);
+		
+		// ˆ—F‘«‚µZ
+		int add = in0+in1;
+		// ˆ—Fˆø‚«Z
+		int add = in0-in1;
+		// ˆ—FŠ|‚¯Z
+		int a=in0*in1;
+		// ˆ—FŠ„‚èZ
+		int b=in0/in1;
+		//@ˆ—F—]‚è
+		int c=in0%in1;
+		
+		//@ŒvZŒ‹‰Ê‚ğo—Í
+		system.out.println("‘«‚µZ:"+add);
+		system.out.println("ˆø‚«Z:"+sub);
+	    system.out.println("Š|‚¯Z:"+a);
+	   	system.out.println("Š„‚èZ:"+b);
+	   	system.out.println("—]‚è"+c);
+	
