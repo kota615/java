1bf139f14b3f9c728dc76285c801441f09a0717a
kota615 <21111153ks@tama.ac.jp>
2014-11-05 15:03:58 +0900

    前回の直しました。確認お願いします。

--- bunki.java
+++ bunki.java
@@ -8,17 +8,27 @@ class bunki
 		int kotae=0;
 		// 入力：数値1、記号、数値２の順番に入力
 		//System.out.print("数値１＝");
-		int in0 = new java.utill.scanner(system.in).nextInt();
+		int in0 = new java.utill.Scanner(System.in).nextInt();
 		System.out.print(
-			"足し算（＋）、引き算（-）、掛け算（＊）、割り算（/)、余り（％）のどれか=");
+			"足し算（+）、引き算（-）、掛け算（*）、割り算（/)、余り（%）のどれか=");
 		// string型の変数kigouに、上記の記号入力
 		String kigou = new java.util.Scanner(System.in).nextLine();
 		System.out.print("数値2=");
-		int in1 = new java.utill.Scanner(system.in).nextInt();
+		int in1 = new java.utill.Scanner(System.in).nextInt();
 	
 		// 処理：記号に応じて計算
 		// ヒント：kigouが+かどうかのif文は以下のとおり
 		// if (kigou.equals("+"))
+		if (kigou.equals("+"))
+			kotae = in0 + in1;
+		if (kigou.equals("-"))
+		    kotae = in0 - in1;
+		if (kigou.equals("*"))
+			kotae = in0 * in1;
+		if (kigou.equals("/"))
+			kotae = in0 / in1;
+		if (kigou.equals("%"))
+			kotae = in0 % in1;
 
 		// 出力：計算結果を出力
 		// 「数値１　計算記号　数値２＝答え」
