b1f479e0d787797aed4931b43ae8143ca5102326
kota615 <21111153ks@tama.ac.jp>
2014-11-05 16:10:20 +0900

    フィボットとリュカ

---
+++ fibo.java
@@ -0,0 +1,26 @@
+class bunki
+{
+
+	public static void main(string argv[])
+	{
+
+		for (int i = 1; i <= 5; i++)
+		{
+			System.out.println(i);
+		}
+
+	    int[] hai = new int[10];
+		lucus[0] = 0;
+		lucus[1] = 10;
+		lucus[2] = 100;
+		System.out.println(lucus[0]+","+lucus[1]+","+lucus[2]);
+
+		for (int i= 0; i<= 9; i++){
+			lucus[i] = i * 2;
+		}
+		for (int i = 0; i <= 9; i++)
+			System.out.println(hai[i]);
+		
+
+	}
+}
